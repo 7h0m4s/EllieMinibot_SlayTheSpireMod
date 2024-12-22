51a4acf5072573acba84a3026b035b5270c3bcc2
thomas pegoraro <thomas.pegoraro@gmail.com>
2024-12-22 19:53:14 +1000

    Refactor ToDo name to EllieMinibot across project

---
+++ src/main/java/EllieMinibot/relics/TodoItem.java
@@ -0,0 +1,13 @@
+package EllieMinibot.relics;
+
+import EllieMinibot.CharacterFile;
+
+import static EllieMinibot.ModFile.makeID;
+
+public class TodoItem extends AbstractEasyRelic {
+    public static final String ID = makeID("TodoItem");
+
+    public TodoItem() {
+        super(ID, RelicTier.STARTER, LandingSound.FLAT, CharacterFile.Enums.ELLIE_MINIBOT_COLOR);
+    }
+}
