f71866d0e5baf1bf212a8fee2e51e3adf7e0e1f4
thomas pegoraro <thomas.pegoraro@gmail.com>
2025-01-11 00:07:00 +1000

    Make drones less talkative

--- src/main/java/EllieMinibot/monsters/EvilDroneMonster.java
+++ src/main/java/EllieMinibot/monsters/EvilDroneMonster.java
@@ -61,8 +61,8 @@ public class EvilDroneMonster extends AbstractMonster {
     }
 
     public void takeTurn() {
-        // 10% chance to say the next message
-        if(lastMessageIndex < DIALOG.length - 1 && new Random().randomBoolean(0.1F)){
+        // 5% chance to say the next message
+        if(lastMessageIndex < DIALOG.length - 1 && new Random().randomBoolean(0.05F)){
             lastMessageIndex += 1;
             AbstractDungeon.actionManager.addToBottom(new TalkAction(this, DIALOG[lastMessageIndex], 0.3F, 2.0F));
 
